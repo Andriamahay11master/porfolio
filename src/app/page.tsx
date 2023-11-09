b4b506afaba17e7bf341ebb8916ce9c669b22e8f
Andriamahay <Henikaja.IRIMANANA@pulse.mg>
2023-11-09 13:49:17 +0300

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 import Image from 'next/image'
+import Banner from '@/components/banner/Banner'
 
 export default function Home() {
+  //Data Banner
+  const dataBanner = {
+    $title: "Hey, I'm MAHAY",
+    $desc: 'A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product',
+    $btn: 'Projects',
+    $linkBtn: '/#projects',
+    $srcImage: '/images/banner/banner.jpg',
+    $width: 1200,
+    $height: 600,
+    $altImage: 'Banner front'
+  }
   return (
-    <main></main>
+    <main className='main-page'>
+      <Banner {...dataBanner}/>
+    </main>
   )
 }
