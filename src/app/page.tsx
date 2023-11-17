dd6ad16c3d1f606d0e3ab5352da35d61f699420f
Andriamahay <Henikaja.IRIMANANA@pulse.mg>
2023-11-17 11:14:09 +0300

    finish section skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Banner from '@/components/banner/Banner'
 import SectionTitle from '@/components/sectionTitle/SectionTitle'
+import BlockInfo from '@/components/blockInfo/BlockInfo'
+import ListSkills from '@/components/listSkills/ListSkills'
 
 export default function Home() {
   //Data Banner
@@ -17,13 +19,63 @@ export default function Home() {
 
   //Data Section Title About
   const dataAbout = {
-    $title: 'ABOUT ME',
+    $title: 'About me',
     $desc: 'Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology'
+  }
+
+  //Data Section Title Project
+  const dataProject = {
+    $title: 'Projects',
+    $desc: 'Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology'
+  }
+
+  //Data Block Info
+  const dataBlockInfo = {
+    $title: 'Front developer',
+    $desc: `👋 Hello World! I'm Mahay, a passionate front-end developer with a knack for transforming ideas into captivating digital experiences. 💻✨ <br/><br/>
+            🚀 Armed with a creative mindset and a love for clean, efficient code, I thrive on the challenges of bringing user interfaces to life. From crafting seamless user journeys to ensuring pixel-perfect design implementation, I am dedicated to delivering web solutions that not only meet but exceed expectations. <br/><br/>
+            🎨 My toolkit includes the latest front-end technologies such as HTML5, CSS3, and JavaScript, and I'm always eager to stay ahead of the curve with emerging trends like React.js and Vue.js. I believe in the power of responsive design, ensuring that the user experience remains flawless across devices. <br/><br/>
+            🛠️ Whether it's optimizing website performance, enhancing interactivity, or collaborating with cross-functional teams, I'm committed to creating web applications that resonate with users and leave a lasting impression.  <br/><br/> 
+            💡 Let's build something extraordinary together – where design meets functionality, and user satisfaction takes center stage. Feel free to reach out for collaborations, ideas, or just a good coding chat! 🚀🌐`,
+    $linkBtn: '/#projects',
+    $valBtn: 'Projects'
+  }
+
+  //Data list skills
+  const dataListSkills = {
+    $title: 'My skills',
+    data: [
+      'HTML',
+      'CSS',
+      'SCSS',
+      'Responsive Design',
+      'SEO',
+      'JavaScript',
+      'Tailwind',
+      'Bootstrap',
+      'Jquery',
+      'React',
+      'NextJS',
+      'VueJS',
+      'Figma',
+      'Git',
+      'SQL',
+      'Office 365',
+    ]
   }
   return (
     <main className='main-page'>
       <Banner {...dataBanner}/>
       <SectionTitle {...dataAbout}/>
+      <div className="main-section about-section">
+        <div className="container">
+          <div className="about-block">
+            <BlockInfo {...dataBlockInfo}/>
+            <ListSkills $title={dataListSkills.$title} data={dataListSkills.data}/>
+          </div>
+        </div>
+      </div>
+      <SectionTitle {...dataProject}/>
     </main>
   )
 }
--- src/components/sectionTitle/SectionTitle.tsx
+++ src/components/sectionTitle/SectionTitle.tsx
@@ -10,8 +10,10 @@ interface SectionTitleProps {
 export default function SectionTitle({$title, $desc} : SectionTitleProps) {
     return (
         <div className="section-title-block">
-            <h2 className="section-title">{$title}</h2>
-            <p>{$desc}</p>
+            <div className="container">
+                <h2 className="section-title">{$title}</h2>
+                <p>{$desc}</p>
+            </div>
         </div>
     )
   }
