c4789b35ad1c9956eadb7f507f5b48abbe9ccb35
Andriamahay <Henikaja.IRIMANANA@pulse.mg>
2023-11-20 09:56:55 +0300

    finalisation section contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -5,23 +5,28 @@ import './contact.scss';
 
 export default function Contact() {
     return (
-        <form className="form-content">
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input className="form-control" type="text" name="name" id="name" />
+        <div className="form-block">
+            <div className="container">
+                <form className="form-content">
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input className="form-control" type="text" name="name" id="name" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input className="form-control" type="email" name="email" id="email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Email</label>
+                        <textarea className="form-control textarea-control" name="message" id="message" cols={30} rows={10}></textarea>
+                    </div>
+                    <div className="form-group form-group-btn">
+                        <button type="submit" className='btn btn-primary btn-send' aria-label='submit form'>submit</button>
+                    </div>
+                </form>
             </div>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input className="form-control" type="email" name="email" id="email" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="message">Email</label>
-                <textarea className="form-control textarea-control" name="message" id="message" cols={30} rows={10}></textarea>
-            </div>
-            <div className="form-group form-group-btn">
-                <button type="submit" className='btn btn-primary btn-send' aria-label='submit form'>submit</button>
-            </div>
-        </form>
+        </div>
+        
     )
   }
   
