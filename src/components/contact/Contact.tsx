4fc42e23f244a73cec9ce6e1dd3c4d76b91f32c4
Andriamahay <Henikaja.IRIMANANA@pulse.mg>
2023-11-25 14:40:45 +0300

    MAJ content projet/contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,26 +2,32 @@
 import * as React from 'react';
 import './contact.scss';
 
+interface ContactProps{
+    name: string,
+    email: string,
+    message: string,
+    valBtn: string
+}
 
-export default function Contact() {
+export default function Contact({name, email, message, valBtn} : ContactProps) {
     return (
         <div className="form-block">
             <div className="container">
                 <form className="form-content">
                     <div className="form-group">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name">{name}</label>
                         <input className="form-control" type="text" name="name" id="name" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="email">{email}</label>
                         <input className="form-control" type="email" name="email" id="email" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="message">Email</label>
+                        <label htmlFor="message">{message}</label>
                         <textarea className="form-control textarea-control" name="message" id="message" cols={30} rows={10}></textarea>
                     </div>
                     <div className="form-group form-group-btn">
-                        <button type="submit" className='btn btn-primary btn-send' aria-label='submit form'>submit</button>
+                        <button type="submit" className='btn btn-primary btn-send' aria-label='submit form'>{valBtn}</button>
                     </div>
                 </form>
             </div>
