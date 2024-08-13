e9cbb5ea2ed19694d774733acefd04aaf5483f2b
Andriamahay11master <hirimanana@yahoo.fr>
2024-08-13 10:12:26 +0300

    init component scrollto top + update icomoon

---
+++ src/components/scrolltotop/ScrollToTop.tsx
@@ -0,0 +1,8 @@
+import './scrolltotop.scss';
+export default function ScrollToTop() {
+    return (
+        <div className="scrolltop">
+            <span className="scrolltop-value"><i className="icon-arrow-up"></i></span>
+        </div>
+    )
+}
