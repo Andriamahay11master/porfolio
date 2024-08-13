c5409da98ffb99728d2e70ed7e9f2c6fb9352f4f
Andriamahay11master <hirimanana@yahoo.fr>
2024-08-13 10:26:39 +0300

    add toggle visibility scrolltop

--- src/components/scrolltotop/ScrollToTop.tsx
+++ src/components/scrolltotop/ScrollToTop.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
 import './scrolltotop.scss';
 export default function ScrollToTop() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 250) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
     return (
-        <div className="scrolltop">
-            <span className="scrolltop-value"><i className="icon-arrow-up"></i></span>
+        <div className={`scrolltop ${isVisible ? 'show' : ''}`} onClick={scrollToTop}>
+            <span className="scrolltop-value">
+                <i className="icon-arrow-up"></i>
+            </span>
         </div>
     )
 }
