51c70c739ecb64b80ef3ea342d376b64c3476d1d
Mahay <hirimanana@yahoo.fr>
2025-09-26 17:44:05 +0400

    udpate components Banner & Project

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,34 +1,54 @@
-"use client"
-import * as React from 'react';
-import Link from 'next/link';
-import './project.scss';
-import Image from 'next/image';
+import "./project.scss";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 interface ProjectProps {
-    $title: string;
-    $desc: string;
-    $btn: string;
-    $linkBtn: string;
-    $srcImage: string;
-    $width: number;
-    $height: number;
-    $altImage: string;
+  $title: string;
+  $desc: string;
+  $btn: string;
+  $linkBtn: string;
+  $srcImage: string;
+  $width: number;
+  $height: number;
+  $altImage: string;
 }
 
-export default function Project({$title, $desc, $btn, $linkBtn, $srcImage, $width, $height, $altImage} : ProjectProps) {
-    return (
-        <div className="project-content">
-            <div className="project-col">
-                <Image className="project-img" src={$srcImage} width={$width} height={$height} alt={$altImage} loading="lazy" title='project image'/>
-            </div>
-            <div className="project-col">
-                <div className="project-info">
-                    <h2 className="title-h2 project-title">{$title}</h2>
-                    <p className='project-desc'>{$desc}</p>
-                    <Link href={$linkBtn} className='btn btn-primary' title='Project link' target='_blank'>{$btn}</Link>
-                </div>
-            </div>
+export default function Project({
+  $title,
+  $desc,
+  $btn,
+  $linkBtn,
+  $srcImage,
+  $width,
+  $height,
+  $altImage,
+}: ProjectProps) {
+  return (
+    <div className="project-content">
+      <div className="project-col">
+        <LazyLoadImage
+          className="project-img"
+          src={$srcImage}
+          width={$width}
+          height={$height}
+          alt={$altImage}
+          loading="lazy"
+          title="project image"
+        />
+      </div>
+      <div className="project-col">
+        <div className="project-info">
+          <h2 className="title-h2 project-title">{$title}</h2>
+          <p className="project-desc">{$desc}</p>
+          <a
+            href={$linkBtn}
+            className="btn btn-primary"
+            title="Project link"
+            target="_blank"
+          >
+            {$btn}
+          </a>
         </div>
-    )
-  }
-  
+      </div>
+    </div>
+  );
+}
