2a21047c0376054337e177c0c613015a00c007c7
Mahay <hirimanana@yahoo.fr>
2025-09-26 17:29:04 +0400

    clean footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,45 +1,61 @@
-import * as React from 'react';
-import './footer.scss';
-import Link from 'next/link';
+import "./footer.scss";
+import { Link } from "react-router-dom";
 
-interface FooterProps{
-    desc: string,
-    title: string,
-    copyright: string
+interface FooterProps {
+  desc: string;
+  title: string;
+  copyright: string;
 }
 
-export default function Footer({desc, title, copyright} : FooterProps) {
-    
-    const currentYear = new Date().getFullYear();
-    return (
-        <footer className='footerPage'>
-            <div className="cntFooter">
-                <div className="colFooter">
-                    <h2 className='title-h2 titleFooter'>IRIMANANA Henikaja Andriamahay </h2>
-                    <p className="txtFooter">
-                        {desc}
-                    </p>
-                </div>
-                <div className="colFooter">
-                    <div className="titleFooter">{title}</div>
-                    <div className="listLink">
-                        <div className="itemLink">
-                            <Link href="https://www.facebook.com/happy.mahay.travellover" target='_blank' aria-label="Profil Facebook d'Andriamahay Henikaja Irimanana" title='Profil Facebook'><i className="icon-footer icon-fb"></i></Link>
-                        </div>
-                        <div className="itemLink">
-                            <Link href="https://www.linkedin.com/in/andriamahay-henikaja-irimanana/" target='_blank' aria-label="Profil LinkedIn d'Andriamahay Henikaja Irimanana" title='Profil LinkedIn'><i className="icon-footer icon-linkedin"></i></Link>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-            <div className="footerCopyright">
-                <div className="cntCopyright">
-                    <div className="itemCol"> 
-                        <p>{currentYear} © {copyright} <Link href="/" className='copyRightLink'>IRIMANANA Henikaja Andriamahay </Link></p>
-                    </div>
-                </div>
+export default function Footer({ desc, title, copyright }: FooterProps) {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="footerPage">
+      <div className="cntFooter">
+        <div className="colFooter">
+          <h2 className="title-h2 titleFooter">
+            IRIMANANA Henikaja Andriamahay{" "}
+          </h2>
+          <p className="txtFooter">{desc}</p>
+        </div>
+        <div className="colFooter">
+          <div className="titleFooter">{title}</div>
+          <div className="listLink">
+            <div className="itemLink">
+              <a
+                href="https://www.facebook.com/happy.mahay.travellover"
+                target="_blank"
+                aria-label="Profil Facebook d'Andriamahay Henikaja Irimanana"
+                title="Profil Facebook"
+              >
+                <i className="icon-footer icon-fb"></i>
+              </a>
             </div>
-        </footer> 
-    )
-  }
-  
+            <div className="itemLink">
+              <a
+                href="https://www.linkedin.com/in/andriamahay-henikaja-irimanana/"
+                target="_blank"
+                aria-label="Profil LinkedIn d'Andriamahay Henikaja Irimanana"
+                title="Profil LinkedIn"
+              >
+                <i className="icon-footer icon-linkedin"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footerCopyright">
+        <div className="cntCopyright">
+          <div className="itemCol">
+            <p>
+              {currentYear} © {copyright}{" "}
+              <Link to="/" className="copyRightLink">
+                IRIMANANA Henikaja Andriamahay{" "}
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
