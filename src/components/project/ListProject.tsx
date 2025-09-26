17ee6ca6fd4f542f0c37947a48121b48c98fa5e7
Mahay <hirimanana@yahoo.fr>
2025-09-26 17:36:46 +0400

    correct component ListProject

--- src/components/project/ListProject.tsx
+++ src/components/project/ListProject.tsx
@@ -1,19 +1,17 @@
-"use client"
-import * as React from 'react';
-import './project.scss';
-import Project from './Project';
-import { ProjectType } from '@/models/ProjectType';
+import "./project.scss";
+import Project from "./Project";
+import type { ProjectType } from "../../models/ProjectType";
 
 interface ListProjectProps {
-    list: ProjectType[]
+  list: ProjectType[];
 }
 
-export default function ListProject({list} : ListProjectProps) {
-    return (
-        <div className='listProject'>
-            {list.map((item, index) => (
-                <Project key={index} {...item} />
-            ))}
-        </div>
-    )
-  }
+export default function ListProject({ list }: ListProjectProps) {
+  return (
+    <div className="listProject">
+      {list.map((item, index) => (
+        <Project key={index} {...item} />
+      ))}
+    </div>
+  );
+}
