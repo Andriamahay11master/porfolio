251e0656cc2f611a4cd44ddbcad69428d60e1813
Mahay <hirimanana@yahoo.fr>
2025-09-26 17:57:22 +0400

    change main component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./header.scss";
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
+import "./assets/scss/main.scss";
 import App from "./App.tsx";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
