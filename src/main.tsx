4a127f033d6eec632bca985d33b0ddecc784e2da
Mahay <hirimanana@yahoo.fr>
2025-09-26 18:44:00 +0400

    add fonts + correction path font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
-import "./assets/scss/main.scss";
 import App from "./App.tsx";
 import { StrictMode } from "react";
 
