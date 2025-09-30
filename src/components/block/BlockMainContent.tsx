58885126b64c2e0ff9619696ca03fa362c746044
Mahay <hirimanana@yahoo.fr>
2025-10-01 01:15:20 +0400

    add animation image banner

--- src/components/block/BlockMainContent.tsx
+++ src/components/block/BlockMainContent.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import type { BlockContent } from "../../models/BlockContent";
 import "./block_content.scss";
+import { useState } from "react";
 interface BlockMainContentProps {
   data: BlockContent;
 }
 export default function BlockMainContent({ data }: BlockMainContentProps) {
+  const [mouseEnterButton, setMouseEnterButton] = useState(false);
   return (
     <div className="block-content">
       <div className="block-col">
         {data.srcImage && (
-          <div className="block-img">
+          <div
+            className={mouseEnterButton ? "block-img animated" : "block-img"}
+          >
             <img
               src={data.srcImage}
               width={data.width}
@@ -25,7 +29,12 @@ export default function BlockMainContent({ data }: BlockMainContentProps) {
           {data.subtitle && <h2 className="title-h2">{data.subtitle}</h2>}
           <p>{data.desc}</p>
           {data.linkBtn && (
-            <Link to={data.linkBtn} className="btn btn-primary">
+            <Link
+              to={data.linkBtn}
+              className="btn btn-primary"
+              onMouseEnter={() => setMouseEnterButton(true)}
+              onMouseLeave={() => setMouseEnterButton(false)}
+            >
               {data.valBtn}
             </Link>
           )}
