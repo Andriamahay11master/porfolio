288672d640b0c90c81630d1bbea2b0f674096dc5
Mahay <hirimanana@yahoo.fr>
2025-10-01 00:54:53 +0400

    change design loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -5,14 +5,7 @@ export default function Loader() {
   return (
     <div className="loader">
       <svg className="spinner" viewBox="0 0 50 50">
-        <circle
-          className="path"
-          cx="25"
-          cy="25"
-          r="20"
-          fill="none"
-          strokeWidth="5"
-        ></circle>
+        <circle className="path" cx="25" cy="25" r="20" fill="none"></circle>
       </svg>
     </div>
   );
