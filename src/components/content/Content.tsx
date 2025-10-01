5ea07d8db03fe9b52163f9059b575b7f1bb6ebec
Mahay <hirimanana@yahoo.fr>
2025-10-01 09:40:26 +0400

    update component Content

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,8 +1,16 @@
-export default function Content({}: any) {
+import "./content.scss";
+
+interface ContentProps {
+  children: React.ReactNode;
+  title: string;
+}
+export default function Content({ children, title }: ContentProps) {
   return (
     <div className="content">
-      <div className="content-col"></div>
-      <div className="content-col"></div>
+      <div className="content-col">
+        <h2 className="title-h2">{title}</h2>
+      </div>
+      <div className="content-col">{children}</div>
     </div>
   );
 }
